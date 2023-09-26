1ee6d8d450d8fb000f2884ad6e2d982a46faabed
Tiago Queiroz <tiagohenriquequeiroz@MacBook-Air-de-Tiago.local>
2023-09-26 10:07:36 -0300

    add animations on drag

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './styles.module.css';
 
 interface TaskProps {
@@ -9,23 +10,38 @@ interface TaskProps {
 }
 
 export function Task({ title, colIndex, taskIndex, taskId , colId}: TaskProps) {
-
+    const [style, setStyle] = useState({});
     const handleOnDrag = (
       e: React.DragEvent<HTMLDivElement>,
       taskId: number,
       colId: number
     ) => {
+        const styles = {
+          transform: "rotate(3deg)",
+          transition: "transform 0.5s",
+        };
+      setStyle(styles)
       e.dataTransfer.setData(
         "text",
         JSON.stringify({ taskIndex, prevColIndex: colIndex, taskId, colId })
       );
     };
 
+    const handleOnDragEnd = () => {
+        const styles = {
+          transform: "rotate(0deg)",
+        };
+        setStyle(styles);
+    }
+
   return (
     <div
+      id="task-drop"
       draggable
+      onDragEnd={handleOnDragEnd}
       onDragStart={(e) => handleOnDrag(e, taskId, colId)}
       className={styles.wrapper}
+      style={style}
     >
       <p className={styles.title}>{title}</p>
     </div>
